519726b645054f1ee42ff88dd962a39e5a45dbec
chai2010 <chaishushan@gmail.com>
2019-11-29 00:10:16 +0800

    存档

---
+++ api_db.go
@@ -0,0 +1,10 @@
+// 以太坊山寨RPC客户端 版权 @2019 柴树杉。
+
+package ethrpc
+
+/*
+db_putString
+db_getString
+db_putHex
+db_getHex
+*/
---
+++ api_ssh.go
@@ -0,0 +1,16 @@
+// 以太坊山寨RPC客户端 版权 @2019 柴树杉。
+
+package ethrpc
+
+/*
+shh_post
+shh_version
+shh_newIdentity
+shh_hasIdentity
+shh_newGroup
+shh_addToGroup
+shh_newFilter
+shh_uninstallFilter
+shh_getFilterChanges
+shh_getMessages
+*/
