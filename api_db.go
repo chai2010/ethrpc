80b5b298fa19af2d2c1ec7361d95407f1d4cf092
chai2010 <chaishushan@gmail.com>
2019-11-29 00:18:14 +0800

    增加DB接口定义

--- api_db.go
+++ api_db.go
@@ -2,9 +2,40 @@
 
 package ethrpc
 
-/*
-db_putString
-db_getString
-db_putHex
-db_getHex
-*/
+// 数据库接口
+type DbAPI interface {
+	PutString(host string, dbname, key, value string) (bool, error)
+	GetString(host string, dbname, key string) (string, error)
+	PutHex(host string, dbname, key, value string) (bool, error)
+	GetHex(host string, dbname, key string) (string, error)
+}
+
+// 构造数据库接口
+func NewDbAPI(host string) DbAPI {
+	return nil
+}
+
+// 保存字符串到本地数据库
+func DbPutString(host string, dbname, key, value string) (bool, error) {
+	// db_putString
+	panic("todo")
+}
+
+// 从本地数据库读取字符串数据
+func DbGetString(host string, dbname, key string) (string, error) {
+	// db_getString
+	panic("todo")
+}
+
+// 保存十六进制字符串到本地数据库
+func DbPutHex(host string, dbname, key, value string) (bool, error) {
+	// ^0[xX][0-9a-fA-F]+$
+	// db_putHex
+	panic("todo")
+}
+
+// 从本地数据库读取十六进制字符串数据
+func DbGetHex(host string, dbname, key string) (string, error) {
+	// db_getHex
+	panic("todo")
+}
